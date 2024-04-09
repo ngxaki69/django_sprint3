e0ac87fd323a1715f6ce0cadee3fd51bc894b409
ngxaki69 <makhnach.den@gmail.com>
2024-04-09 09:37:32 +0300

    'Update'

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
-from .models import Post, Category
+from .models import Category, Post
 from .constants import TOTAL_POSTS
 
 
